0e1138d2f3da04c56fe22da0aadb84df1a9e102e
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-10-30 15:12:34 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,42 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmakaro <anmakaro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 12:19:37 by anmakaro          #+#    #+#             */
+/*   Updated: 2023/10/30 14:53:22 by anmakaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-char	*ft_read_and_append(int fd, char *buffer, char *saved_str)
+char	*ft_read_and_append(int fd, char *saved_str)
 {
 	char	*temp;
 	int		bytes_read;
 
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read == -1)
-	{
-		free(buffer);
+	temp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!temp)
 		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr_v(saved_str, '\n') && bytes_read != 0)
+	{
+		bytes_read = read(fd, temp, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(temp);
+			return (NULL);
+		}
+		temp[bytes_read] = '\0';
+		saved_str = ft_strjoin_v(saved_str, temp);
 	}
-	buffer[bytes_read] = '\0';
-	temp = ft_strjoin(saved_str, buffer);
-	free(saved_str);
-	return (temp);
+	free(temp);
+	return (saved_str);
 }
+
+
 
 char	*get_next_line(int fd)
 {
 	char		*line;
 	static char	*saved_str;
-	char		*buffer;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-
-	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
-		return (NULL);
-
-	saved_str = ft_read_and_append(fd, buffer, saved_str);
+	saved_str = ft_read_and_append(fd, saved_str);
 	if (!saved_str)
 		return (NULL);
-
-	line = ft_get_line_from_left_str(saved_str);
-	saved_str = ft_update_left_str(saved_str);
+	line = ft_getline_fromleftstr(saved_str);
+	saved_str = ft_update_leftstr(saved_str);
 	return (line);
 }
 
@@ -51,12 +65,12 @@ int	main(void)
 	char	*line;
 	int		line_num;
 	int		fd1;
-	int		fd2;
-	int		fd3;
+	//int		fd2;
+	//int		fd3;
 
 	fd1 = open("tests/test1.txt", O_RDONLY);
-	fd2 = open("tests/test2.txt", O_RDONLY);
-	fd3 = open("tests/test3.txt", O_RDONLY);
+	//fd2 = open("tests/test2.txt", O_RDONLY);
+	//fd3 = open("tests/test3.txt", O_RDONLY);
 
 	line_num = 1;
 	while (line_num < 7)
@@ -65,20 +79,20 @@ int	main(void)
 		printf("line [%02d] from fd1: %s\n", line_num, line);
 		free(line);
 
-		line = get_next_line(fd2);
-		printf("line [%02d] from fd2: %s\n", line_num, line);
-		free(line);
+		//line = get_next_line(fd2);
+		//printf("line [%02d] from fd2: %s\n", line_num, line);
+		//free(line);
 
-		line = get_next_line(fd3);
-		printf("line [%02d] from fd3: %s\n", line_num, line);
-		free(line);
+		//line = get_next_line(fd3);
+		//printf("line [%02d] from fd3: %s\n", line_num, line);
+		//free(line);
 
 		line_num++;
 	}
 
 	close(fd1);
-	close(fd2);
-	close(fd3);
+	//close(fd2);
+	//close(fd3);
 
 	return (0);
 }
