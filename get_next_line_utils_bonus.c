5c7420ff0f352a6b0f168834e11d02a063c098b2
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-10-31 14:28:45 +0100

    Update get_next_line_utils_bonus.c

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmakaro <anmakaro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 12:19:58 by anmakaro          #+#    #+#             */
+/*   Updated: 2023/10/30 16:05:47 by anmakaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen_v(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr_v(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (c == '\0')
+		return ((char *)&s[ft_strlen_v(s)]);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin_v(char *left_str, char *buff)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!left_str)
+	{
+		left_str = (char *)malloc(1 * sizeof(char));
+		left_str[0] = '\0';
+	}
+	if (!left_str || !buff)
+		return (NULL);
+	str = malloc(sizeof(char) * ((ft_strlen_v(left_str)
+					+ ft_strlen_v(buff)) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (left_str)
+		while (left_str[++i] != '\0')
+			str[i] = left_str[i];
+	while (buff[j] != '\0')
+		str[i++] = buff[j++];
+	str[ft_strlen_v(left_str) + ft_strlen_v(buff)] = '\0';
+	free(left_str);
+	return (str);
+}
+
+char	*ft_getline_fromleftstr(char *left_str)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!left_str[i])
+		return (NULL);
+	while (left_str[i] && left_str[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (left_str[i] && left_str[i] != '\n')
+	{
+		line[i] = left_str[i];
+		i++;
+	}
+	if (left_str[i] == '\n')
+	{
+		line[i] = left_str[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_update_leftstr(char *left_str)
+{
+	int		i;
+	int		j;
+	char	*new_left_str;
+
+	i = 0;
+	while (left_str[i] && left_str[i] != '\n')
+		i++;
+	if (!left_str[i])
+	{
+		free(left_str);
+		return (NULL);
+	}
+	new_left_str = (char *)malloc(sizeof(char)
+			* (ft_strlen_v(left_str) - i + 1));
+	if (!new_left_str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (left_str[i])
+		new_left_str[j++] = left_str[i++];
+	new_left_str[j] = '\0';
+	free(left_str);
+	return (new_left_str);
+}
