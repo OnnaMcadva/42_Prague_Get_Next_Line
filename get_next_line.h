a36433be5169cd7e42de526b202a000ae748c70e
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-10-31 14:19:38 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -10,13 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-#include <unistd.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
